a918d27e0bec2221558f7f1387265739f0f8d5f6
Jacson Junior <jacson.g.junior@gmail.com>
2020-06-20 16:36:56 -0300

    premake file

---
+++ premake5.lua
@@ -0,0 +1,55 @@
+project "GLFW"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/openpnp-capture.h",
+		"src/common/**.h",
+		"src/common/**.cpp"
+	}
+	filter "system:macosx"
+		pic "On"
+
+		systemversion "latest"
+		staticruntime "On"
+
+		files
+		{
+			"mac/platformcontext.h",
+			"mac/platformcontext.mm",
+			"mac/platformdeviceinfo.h",
+			"mac/platformstream.h",
+			"mac/platformstream.mm",
+			"mac/uvcctrl.h",
+			"mac/uvcctrl.mm",
+        }
+        
+        libdirs {
+            os.findlib("AVFoundation"),
+            os.findlib("Foundation"),
+            os.findlib("CoreMedia"),
+            os.findlib("CoreVideo"),
+            os.findlib("Accelerate"),
+            os.findlib("IOKit"),
+        }
+
+        links {
+            "AVFoundation.framework",
+            "Foundation.framework",
+            "CoreMedia.framework",
+            "CoreVideo.framework",
+            "Accelerate.framework",
+            "IOKit.framework",
+        }
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
